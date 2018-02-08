59dd63a86c25e4c3ca6d2ad2148e5355346a289e
s448766 <36240560+s448766@users.noreply.github.com>
2018-02-08 14:33:58 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created by : Md Faiyaz Hossain
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+display.setDefault( "background", 0, 0, 1 )
+
+local image = display.newImageRect( "Walk (4).png", 1000, 1000 )
+image.x = display.contentCenterX
+image.y = display.contentCenterY
